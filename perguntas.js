4e3a70932f92a86888fc322bf30c4847f04c297e
eloahlisboa <c.eloah@escola.pr.gov.br>
2025-05-12 14:45:38 +0000

    ok

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao (
+'programação',
+'o que é JavaScript?',
+'linguagem de programação'
+)
+
+criaCartao (
+'inglês',
+'como se escreve dia do pagamento?',
+'pay day'
+)
+
+criaCartao (
+'matemática',
+'qual o gráfico da função quadrática?',
+'parábola'
+)
